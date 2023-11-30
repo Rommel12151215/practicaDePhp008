4f9c5898edcf1d8c96c4adf085c0a37026f8b915
Rommel <karlsrommel255@gmail.com>
2023-11-30 14:46:44 -0600

    practicaDePhp008

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Formulario de Registro</h2>
+
+<form action="registro.php" method="post">
+ Nombre:<br>
+ <input type="text" name="nombre">
+ <br>
+ Email:<br>
+ <input type="text" name="email">
+ <br><br>
+ <input type="submit" value="Registrarse">
+</form> 
+
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+
+// Aquí iría el código para almacenar $nombre y $email en una base de datos
+
+echo "Gracias por registrarte, " . $nombre . "!";
+?>
